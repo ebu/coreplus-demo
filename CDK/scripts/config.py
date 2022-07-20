dedafab23f3f6f3768ba5b9892dcce2658b8e92e
Mohsin Ali <mohsin.ali.ext@sem42.com>
2022-07-20 14:07:01 +0500

    Updated the Notebooks

--- CDK/scripts/config.py
+++ CDK/scripts/config.py
@@ -35,8 +35,7 @@ SHORT_LEGEND = 'https://res.cloudinary.com/alieymsxxn/image/upload/v1655326772/l
 ITABLE_TITLE = '<h4 style="text-alignt:center"><label style="font-weight:bold">Table: </label><label>{title}</label></h4>'
 
 # Config vars for APIs
-TENANT = 'Demo-UUID'
-URL = 'http://host.docker.internal'
+URL = 'https://ebu-cdk.innotrade.com'
 ENAPSO_NAMESPACE = '/enapso-dev/ontology-management'
 GRAPH = 'http://www.ebu.ch/metadata/ontologies/ebucore'
 INDIVIDUAL_NAMESPACE = '/enapso-dev/individual-management'
